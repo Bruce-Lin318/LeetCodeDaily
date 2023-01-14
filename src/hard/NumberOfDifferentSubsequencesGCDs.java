eae8043330d1a65015cbe36b128d70a62497eb33
Bruce-Lin318 <22322619@qq.com>
2023-01-14 22:41:04 +0800

    1819. 序列中不同最大公约数的数目

---
+++ src/hard/NumberOfDifferentSubsequencesGCDs.java
@@ -0,0 +1,48 @@
+package hard;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.cn/problems/number-of-different-subsequences-gcds/
+ * 1819. 序列中不同最大公约数的数目
+ *
+ * @author Lin
+ * @date 2023/1/14
+ */
+@SuppressWarnings("AlibabaClassNamingShouldBeCamel")
+public class NumberOfDifferentSubsequencesGCDs {
+    public int countDifferentSubsequenceGCDs(int[] nums) {
+        int maxVal = Arrays.stream(nums).max().getAsInt();
+        boolean[] occured = new boolean[maxVal + 1];
+        for (int num : nums) {
+            occured[num] = true;
+        }
+        int ans = 0;
+        for (int i = 1; i <= maxVal; i++) {
+            int subGcd = 0;
+            for (int j = i; j <= maxVal; j += i) {
+                if (occured[j]) {
+                    if (subGcd == 0) {
+                        subGcd = j;
+                    } else {
+                        subGcd = gcd(subGcd, j);
+                    }
+                    if (subGcd == i) {
+                        ans++;
+                        break;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    public int gcd(int num1, int num2) {
+        while (num2 != 0) {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp % num2;
+        }
+        return num1;
+    }
+}
