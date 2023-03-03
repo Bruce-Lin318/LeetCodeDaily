de8a9348ddf1420a5b9d1465f90db4915927f153
Bruce-Lin318 <22322619@qq.com>
2023-03-03 22:27:40 +0800

    1487. 保证文件名唯一

---
+++ src/medium/MakingFileNamesUnique.java
@@ -0,0 +1,39 @@
+package medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.cn/problems/making-file-names-unique/
+ * 1487. 保证文件名唯一
+ *
+ * @author Lin
+ * @date 2023/3/3
+ */
+public class MakingFileNamesUnique {
+
+    public String[] getFolderNames(String[] names) {
+        Map<String, Integer> map = new HashMap<>();
+        String[] ans = new String[names.length];
+        for (int i = 0; i < names.length; i++) {
+            String name = names[i];
+            if (map.containsKey(name)) {
+                int num = map.get(name);
+                while (map.containsKey(addSuffix(name, num))) {
+                    num++;
+                }
+                ans[i] = addSuffix(name, num);
+                map.put(name, num + 1);
+                map.put(ans[i], 1);
+            } else {
+                ans[i] = name;
+                map.put(name, 1);
+            }
+        }
+        return ans;
+    }
+
+    private String addSuffix(String name, int num) {
+        return name + "(" + num + ")";
+    }
+}
