10512cf20eb172c0c0d63ae747fc089bd05ac9a8
Lin_ZhengHao <22322619@qq.com>
2023-07-01 22:39:04 +0800

    1. 两数之和

---
+++ src/easy/TwoSum.java
@@ -0,0 +1,25 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.cn/problems/two-sum/
+ * 1. 两数之和
+ *
+ * @author Lin
+ * @date 2023/7/1
+ */
+public class TwoSum {
+  public int[] twoSum(int[] nums, int target) {
+    Map<Integer,Integer> map = new HashMap();
+    for(int i = 0 ;i < nums.length;i++){
+      if(map.containsKey(target - nums[i])){
+        return new int[]{map.get(target - nums[i]),i};
+      }
+      map.put(nums[i],i);
+    }
+
+    return new int[0];
+  }
+}
