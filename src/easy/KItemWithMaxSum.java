f4de687352d789d338e569898da7e09ee809cb9d
Lin_ZhengHao <22322619@qq.com>
2023-07-05 09:26:11 +0800

    2600. K 件物品的最大和

--- src/easy/KItemWithMaxSum.java
+++ src/easy/KItemWithMaxSum.java
@@ -8,8 +8,8 @@ package easy;
  * @date 2023/7/5
  */
 public class KItemWithMaxSum {
-  public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
-    return k > numOnes + numZeros ? numOnes - (k - numOnes - numZeros) : Math.min(numOnes, k);
-  }
+    public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        return k > numOnes + numZeros ? numOnes - (k - numOnes - numZeros) : Math.min(numOnes, k);
+    }
 
 }
